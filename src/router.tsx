af904a5b08b71e88b2f2f3ea425a0896ea6ef656
Maglin <mxavier222@gmail.com>
2026-06-04 17:32:16 +0200

    Fix GitHub Pages router basepath

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,13 @@
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
-export const router = createRouter({ routeTree });
+export const router = createRouter({
+  routeTree,
+  basepath: "/Smart-Diet-Planner",
+});
 
 declare module "@tanstack/react-router" {
-	interface Register {
-		router: typeof router;
-	}
+  interface Register {
+    router: typeof router;
+  }
 }
